a2165c224d8def702b69487305f889857b7f36e4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-12-22 22:01:02 -0800

    Added a bg texture
    I like it

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -12,6 +12,16 @@ public partial class Game : Node2D {
             GetNode<Sprite2D>($"Ground/Texture/Ground{i}").Modulate = new Color(GroundRed / 255f, GroundGreen / 255f, GroundBlue / 255f);
         }
 
+        // Sets the background color
+        int BackGroundRed = 0;
+        int BackGroundGreen = 0;
+        int BackGroundBlue = 100;
+
+        for (int i = 0; i <= 2; i++) {
+            GetNode<Sprite2D>($"Background/BG{i}").Modulate = new Color(BackGroundRed / 255f, BackGroundGreen / 255f, BackGroundBlue / 255f);
+        }
+
+        // Loops the ground
         for (int i = 0; i <= 8; i++) {
             Sprite2D Sprite = GetNode<Sprite2D>($"Ground/Texture/Ground{i}");
 
@@ -19,5 +29,16 @@ public partial class Game : Node2D {
                 Sprite.GlobalPosition += new Vector2((281.6f * 9f) - (7 * 9), 0);
             }
         }
+
+        // Loops the bg
+        for (int i = 0; i <= 2; i++) {
+            Sprite2D Sprite = GetNode<Sprite2D>($"Background/BG{i}");
+
+            if (Sprite.GlobalPosition.X < GetNode<Camera2D>("Camera2D").GlobalPosition.X - 1920) {
+                Sprite.GlobalPosition += new Vector2(1920 * 2, 0);
+            }
+        }
+
+        GetNode<Node2D>("Background").Position += new Vector2(600.0f * (float)delta, 0);
     }
 }
