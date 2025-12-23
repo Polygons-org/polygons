16090310b43406121be5af1d349dc6ae7665004d
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-12-22 22:06:50 -0800

    Moved bg and ground colors to globals.cs
    globals was literally empty this whole time lmao

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -3,22 +3,14 @@ using System;
 
 public partial class Game : Node2D {
     public override void _Process(double delta) {
-        // Sets the ground color
-        int GroundRed = 0;
-        int GroundGreen = 0;
-        int GroundBlue = 100;
 
         for (int i = 0; i <= 8; i++) {
-            GetNode<Sprite2D>($"Ground/Texture/Ground{i}").Modulate = new Color(GroundRed / 255f, GroundGreen / 255f, GroundBlue / 255f);
+            GetNode<Sprite2D>($"Ground/Texture/Ground{i}").Modulate = new Color(Globals.GroundRed / 255f, Globals.GroundGreen / 255f, Globals.GroundBlue / 255f);
         }
 
-        // Sets the background color
-        int BackGroundRed = 0;
-        int BackGroundGreen = 0;
-        int BackGroundBlue = 100;
 
         for (int i = 0; i <= 2; i++) {
-            GetNode<Sprite2D>($"Background/BG{i}").Modulate = new Color(BackGroundRed / 255f, BackGroundGreen / 255f, BackGroundBlue / 255f);
+            GetNode<Sprite2D>($"Background/BG{i}").Modulate = new Color(Globals.BackGroundRed / 255f, Globals.BackGroundGreen / 255f, Globals.BackGroundBlue / 255f);
         }
 
         // Loops the ground
--- Scripts/Globals.cs
+++ Scripts/Globals.cs
@@ -2,5 +2,11 @@ using Godot;
 using System;
 
 public partial class Globals : Node {
-    // public static 
+    public static int GroundRed = 0;
+    public static int GroundGreen = 0;
+    public static int GroundBlue = 100;
+
+    public static int BackGroundRed = 0;
+    public static int BackGroundGreen = 0;
+    public static int BackGroundBlue = 100;
 }
