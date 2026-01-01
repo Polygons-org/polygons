3538a19764ed80bb4c9d6d3e81a31706459a790b
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-01-01 00:12:45 -0800

    Added a ground to the editor
    I'll probably not make this loop in the editor either because again I
    just can't be bothered and imo it doesn't really matter much in the
    editor.

--- Scripts/Editor.cs
+++ Scripts/Editor.cs
@@ -4,5 +4,9 @@ using System;
 public partial class Editor : Node2D {
     public override void _Ready() {
         GetNode<Sprite2D>($"Background").Modulate = new Color(Globals.BackGroundRed / 255f, Globals.BackGroundGreen / 255f, Globals.BackGroundBlue / 255f);
+
+        for (int i = 0; i <= 8; i++) {
+            GetNode<Sprite2D>($"Ground/Ground{i}").Modulate = new Color(Globals.GroundRed / 255f, Globals.GroundGreen / 255f, Globals.GroundBlue / 255f);
+        }
     }
 }
