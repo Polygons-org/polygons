73bf98a9d6b484d7af1348448f26c26d1d0d8ddf
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-01-12 19:29:05 -0800

    Made it so you can pan around the camera in the editor
    I genuinely cant be bothered to make it so the bg and ground scroll
    properly so I'll do that either way later or never

--- Scripts/Editor.cs
+++ Scripts/Editor.cs
@@ -1,12 +1,41 @@
 using Godot;
 using System;
+using System.Security.AccessControl;
 
 public partial class Editor : Node2D {
+    int CameraSpeed = 10;
+
     public override void _Ready() {
         GetNode<Sprite2D>($"Background").Modulate = new Color(Globals.BackGroundRed / 255f, Globals.BackGroundGreen / 255f, Globals.BackGroundBlue / 255f);
 
         for (int i = 0; i <= 8; i++) {
             GetNode<Sprite2D>($"Ground/Ground{i}").Modulate = new Color(Globals.GroundRed / 255f, Globals.GroundGreen / 255f, Globals.GroundBlue / 255f);
+        }
+    }
+
+    public override void _Process(double delta) {
+        if (Input.IsKeyPressed(Key.Shift)) {
+            CameraSpeed = 30;
+        } else {
+            CameraSpeed = 10;
+        }
+
+        if (Input.IsKeyPressed(Key.W)) {
+            GetNode<Camera2D>("Camera2D").Position += new Vector2(0, -CameraSpeed);
+            GetNode<ColorRect>("StartLine").Position += new Vector2(0, -CameraSpeed);
+            GetNode<Sprite2D>("Background").Position += new Vector2(0, -CameraSpeed);
+        } if (Input.IsKeyPressed(Key.S)) {
+            GetNode<Camera2D>("Camera2D").Position += new Vector2(0, CameraSpeed);
+            GetNode<ColorRect>("StartLine").Position += new Vector2(0, CameraSpeed);
+            GetNode<Sprite2D>("Background").Position += new Vector2(0, CameraSpeed);
+        } if (Input.IsKeyPressed(Key.A)) {
+            GetNode<Camera2D>("Camera2D").Position += new Vector2(-CameraSpeed, 0);
+            GetNode<Node2D>("Ground").Position += new Vector2(-CameraSpeed, 0);
+            GetNode<Sprite2D>("Background").Position += new Vector2(-CameraSpeed, 0);
+        } if (Input.IsKeyPressed(Key.D)) {
+            GetNode<Camera2D>("Camera2D").Position += new Vector2(CameraSpeed, 0);
+            GetNode<Node2D>("Ground").Position += new Vector2(CameraSpeed, 0);
+            GetNode<Sprite2D>("Background").Position += new Vector2(CameraSpeed, 0);
         }
     }
 }
