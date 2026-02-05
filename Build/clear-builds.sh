b3ef2a9a4d2eec30a8686abb78d0afafbac1a41c
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-02-04 17:28:20 -0800

    Fixed an issue with the build scripts
    I might not be able to make a modding api cuz for mods to compile they
    need Game.dll but that just doesnt exist ig

--- Build/clear-builds.sh
+++ Build/clear-builds.sh
@@ -1,14 +1,14 @@
 rm -rf Build/linux/*
 rm -rf Build/windows/*
 rm -rf Build/mac/*
-rm -rf Build/linux-arm/*
+rm -rf Build/linux-arm64/*
 
 touch Build/linux/.gitkeep
 touch Build/windows/.gitkeep
 touch Build/mac/.gitkeep
-touch Build/linux-arm/.gitkeep
+touch Build/linux-arm64/.gitkeep
 
-rm -rf Build/polygons-linux-arm.zip
+rm -rf Build/polygons-linux-arm64.zip
 rm -rf Build/polygons-linux.zip
 rm -rf Build/polygons-windows.zip
 rm -rf Build/polygons-mac.zip
--- Build/zip-builds.sh
+++ Build/zip-builds.sh
@@ -1,4 +1,4 @@
 (cd Build/linux && 7z a ../../Build/polygons-linux.zip "data_Polygons_linuxbsd_x86_64" polygons.pck polygons.x86_64)
-(cd Build/linux-arm && 7z a ../../Build/polygons-linux-arm.zip "data_Polygons_linuxbsd_arm64" polygons.pck polygons.arm64)
+(cd Build/linux-arm64 && 7z a ../../Build/polygons-linux-arm64.zip "data_Polygons_linuxbsd_arm64" polygons.pck polygons.arm64)
 (cd Build/windows && 7z a ../../Build/polygons-windows.zip "data_Polygons_windows_x86_64" polygons.pck polygons.exe)
 (cd Build/mac && 7z a ../../Build/polygons-mac.zip polygons.app)
