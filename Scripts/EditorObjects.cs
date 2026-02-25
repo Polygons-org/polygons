8012efba9f64a6278f3e98623e62cf75e7215ee4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-02-25 10:34:32 -0800

    Fixed being able to place objects when selecting an object
    It still places an object if you click one of the buttons in the top
    right or the play button but idk how to fix that yet

--- Scripts/EditorObjects.cs
+++ Scripts/EditorObjects.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class EditorObjects : Node2D {
     public override void _Process(double delta) {
-        if (Input.IsActionJustPressed("PlaceObject")) {
-            var MousePos = GetGlobalMousePosition();
+        var MousePos = GetGlobalMousePosition();
+        if (Input.IsActionJustPressed("PlaceObject") && MousePos.Y < 680f) {
             PackedScene Object = GD.Load<PackedScene>("res://Prefabs/Object.tscn");
             var NewObject = Object.Instantiate<Node2D>();
             NewObject.Position = MousePos;
