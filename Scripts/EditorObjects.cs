ea1b7d8b366bc8f9bf1acdd19922095391e53f9b
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-02-27 11:05:15 -0800

    Fixed not being able to place objects under the ground
    This was cuz I was accidently checking if the mouse pos in the world was
    below a certain point and not relative to the screen. It's fixed now.

--- Scripts/EditorObjects.cs
+++ Scripts/EditorObjects.cs
@@ -3,14 +3,15 @@ using System;
 
 public partial class EditorObjects : Node2D {
     public override void _Process(double delta) {
-        var MousePos = GetGlobalMousePosition();
+        var GlobalMousePos = GetGlobalMousePosition();
+        var MousePos = GetViewport().GetMousePosition();
         bool mouseOverUI = GetViewport().GuiGetHoveredControl() != null;
 
         if (Input.IsActionJustPressed("PlaceObject") && MousePos.Y < 680f && !mouseOverUI) {
             PackedScene Object = GD.Load<PackedScene>("res://Prefabs/Object.tscn");
             var NewObject = Object.Instantiate<Node2D>();
             NewObject.GetNode<Sprite2D>("Sprite2D").Texture = GD.Load<Texture2D>("res://Assets/Textures/Objects/" + Globals.SelectedObject + ".png");
-            NewObject.Position = MousePos;
+            NewObject.Position = GlobalMousePos;
             AddChild(NewObject);
         }
     }
