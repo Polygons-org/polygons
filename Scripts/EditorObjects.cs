d5d5ead7ac56e154c0cf0fdb1d46e7d2a16b6154
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-02-27 11:42:00 -0800

    Made objects snap to the grid

--- Scripts/EditorObjects.cs
+++ Scripts/EditorObjects.cs
@@ -3,16 +3,26 @@ using System;
 
 public partial class EditorObjects : Node2D {
     public override void _Process(double delta) {
-        var GlobalMousePos = GetGlobalMousePosition();
-        var MousePos = GetViewport().GetMousePosition();
+        var globalMousePos = GetGlobalMousePosition();
+        var mousePos = GetViewport().GetMousePosition();
         bool mouseOverUI = GetViewport().GuiGetHoveredControl() != null;
 
-        if (Input.IsActionJustPressed("PlaceObject") && MousePos.Y < 680f && !mouseOverUI) {
-            PackedScene Object = GD.Load<PackedScene>("res://Prefabs/Object.tscn");
-            var NewObject = Object.Instantiate<Node2D>();
-            NewObject.GetNode<Sprite2D>("Sprite2D").Texture = GD.Load<Texture2D>("res://Assets/Textures/Objects/" + Globals.SelectedObject + ".png");
-            NewObject.Position = GlobalMousePos;
-            AddChild(NewObject);
+        if (Input.IsActionJustPressed("PlaceObject") && mousePos.Y < 680f && !mouseOverUI) {
+            float gridSize = 120f;
+            Vector2 gridOrigin = new Vector2(0, 666);
+
+            Vector2 snappedPos = new Vector2(
+                Mathf.Round((globalMousePos.X - gridOrigin.X) / gridSize) * gridSize + gridOrigin.X,
+                Mathf.Round((globalMousePos.Y - gridOrigin.Y) / gridSize) * gridSize + gridOrigin.Y
+            );
+
+            PackedScene scene = GD.Load<PackedScene>("res://Prefabs/Object.tscn");
+            var newObject = scene.Instantiate<Node2D>();
+            newObject.GetNode<Sprite2D>("Sprite2D").Texture =
+                GD.Load<Texture2D>("res://Assets/Textures/Objects/" + Globals.SelectedObject + ".png");
+
+            newObject.Position = snappedPos;
+            AddChild(newObject);
         }
     }
 }
