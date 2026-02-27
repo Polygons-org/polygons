aff63c0e734276f0778270f0bba32521e4f3a3db
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2026-02-27 10:47:23 -0800

    Fixed being able to place an object when you click a button
    It was an easy fix actually

--- Scripts/EditorObjects.cs
+++ Scripts/EditorObjects.cs
@@ -4,7 +4,9 @@ using System;
 public partial class EditorObjects : Node2D {
     public override void _Process(double delta) {
         var MousePos = GetGlobalMousePosition();
-        if (Input.IsActionJustPressed("PlaceObject") && MousePos.Y < 680f) {
+        bool mouseOverUI = GetViewport().GuiGetHoveredControl() != null;
+
+        if (Input.IsActionJustPressed("PlaceObject") && MousePos.Y < 680f && !mouseOverUI) {
             PackedScene Object = GD.Load<PackedScene>("res://Prefabs/Object.tscn");
             var NewObject = Object.Instantiate<Node2D>();
             NewObject.GetNode<Sprite2D>("Sprite2D").Texture = GD.Load<Texture2D>("res://Assets/Textures/Objects/" + Globals.SelectedObject + ".png");
